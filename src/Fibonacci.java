180e93a0f22a8fcc64853f2496fcea391495cd5a
szt2otokov <szt2otokov@vasvari.org>
2023-09-29 11:20:03 +0200

    all works!

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,50 +1,56 @@
-import jdk.jshell.spi.ExecutionControl;
-
 import java.util.Scanner;
 
 public class Fibonacci {
+    private static int length;
+    private static int result;
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("add meg a sor hosszát:");
-        int length = scn.nextInt();
+        length = scn.nextInt();
 
-        int fibo = fibonacci(length);
+        int fibo = fibonacci();
         System.out.println("Az eredmény ciklussal: " + fibo);
-        fibo = fibonacciRec(length);
+        fibo = fibonacciRec();
         System.out.println("Az eredmény rekurzívan: " + fibo);
     }
 
-    private static int fibonacci(int n){
+    private static int fibonacci(){
         int current0 = 0;
         int current1 = 1;
-        for(int i = 0; i < n;i++){
+        for(int i = 0; i < length;i++){
             if(current0 >= current1){
                 current1 = current0+current1;
             } else {
                 current0 = current1+current0;
             }
         }
-        if(n % 2 == 0){
-            return current0;
+        if(length % 2 == 0){
+            result = current0;
         } else {
-            return current1;
+            result = current1;
         }
+        return result;
     }
 
-    private static int fibonacciRec(int n){
-        int current0 = 0;
-        int current1 = 1;
-        int it = 0;
-        Runnable iteration = (int c0,int c1,int i) -> {
-            if(i < n) {
-                if(c0 >= c1){
-                    c0 = c0+c1;
-                } else {
-                    c1 = c0+c1;
-                }
+    private static int fibonacciRec(){
+        iteration(0,1,0);
+        return result;
+    }
+
+    private static void iteration(int c0,int c1,int i){
+        if(i < length) {
+            if(c0 >= c1){
+                c1 = c0+c1;
+            } else {
+                c0 = c1+c0;
             }
-        };
-        //todo:finish recursion
-        return 0;
+            i++;
+            iteration(c0,c1,i);
+        } else if(length % 2 == 0){
+            result = c0;
+        } else {
+            result = c1;
+        }
+
     }
 }
